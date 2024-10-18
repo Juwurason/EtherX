b685d1117bafd7c6dbdab07d6d0d021218763e00
Juwurason <ajibolasunday7@gmail.com>
2024-10-18 08:12:48 +0100

    chore: jobs change to add-job

--- src/routes/jobRoutes.ts
+++ src/routes/jobRoutes.ts
@@ -9,7 +9,7 @@ import authenticate from "../middleware/authenticate";
 
 const router = express.Router();
 
-router.post("/jobs", authenticate, createJob);
+router.post("/add-job", authenticate, createJob);
 router.get("/get-jobs", getAllJobs);
 router.post("/apply-job", authenticate, applyForJob);
 router.get("/job/:jobId/applicants", authenticate, getJobApplicants);
